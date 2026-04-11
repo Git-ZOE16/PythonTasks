0c30e4a954647b83a9aef8c095158b985fa273ef
Zoe <zoeinspired1609@gmail.com>
2026-04-11 19:56:33 +0100

    added all Assignment file

---
+++ Assignment /Chapter 2 Na Beans/Markgrade.py
@@ -0,0 +1,15 @@
+mark = int(input("Enter your mark (0-100): "))
+if mark >= 100:
+    print("Grade: A")
+if mark >= 80:
+    print("Grade: B")
+if mark >= 70:
+    print("Grade: C")
+if mark >= 60:
+    print("Grade: D")
+if mark >= 50:
+    print("Grade: E")
+else:
+    print("Grade: F")
+
+
