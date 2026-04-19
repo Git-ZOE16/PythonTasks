4263b4dddc0260d161dcf53d4be21f0c391e18f2
Zoe <zoeinspired1609@gmail.com>
2026-04-19 22:14:04 +0100

    add nokia3310 file

---
+++ ChapterThree/nokia3310.py
@@ -0,0 +1,259 @@
+menu = """
+MENU
+1. PhoneBook
+2. Messages
+3. Chat
+4. Call Register
+5. Tones
+6. Settings
+7. Call Divert
+8. Games
+9. Calculator
+10. Reminder
+11. Clock
+12. Profiles
+13. Sim Services
+0. Exit
+select an option
+"""
+
+#PHONE_BOOK_MENU
+phone_book_menu = """
+PHONEBOOK
+    1. Search
+    2. Service Nos
+    3. Add name
+    4. Erase
+    5. Edit
+    6. Assign tone
+    7. Send b'card'
+    8. Options
+    9. Speed dials
+    10. Voice tags
+    select an option
+    """
+
+#PHONE_BOOK_OPTION_SUBMENU
+phone_book_option_menu = """
+options
+    1. Type of View
+    2. Memory status
+    """
+
+#PHONE_MESSAGE_MENU
+phone_message_menu = """
+MESSAGES
+    1. Write Messages
+    2. Inbox 
+    3. Outbox
+    4. Picture Messagges
+    5. Template
+    6. Similey
+    7. Message Settings
+    8. Info Service
+    9. Voice Mailbox Number
+    10. Service Command Editor
+    select an option
+    """
+
+#MESSAGE_SETTINGS_SUBMENU
+message_settings_menu = """
+MESSAGE_SETTINGS
+    1. Set1
+    2. Common
+    select an option
+    """
+
+#SET1_OPTION_SUBMENU
+set1_menu = """
+SET1_OPTIONS
+    1. Message Center Number
+    2. Messages sent as
+    3. Message validity
+    select an option
+    """
+
+#COMMON_OPTION_SUBMENU
+common_menu = """
+COMMON_OPTIONS
+    1. Delivery Reports
+    2. Delivery Via same center
+    3. Character support
+    select an option
+    """
+
+#CHAT_MENU
+chat_menu = """
+CHAT
+    1. Start a chat
+    """
+
+#CALL_REGISTER_MENU
+call_register_menu = """
+CALL_REGISTER
+    1. Missed calls
+    2. Received Calls
+    3. Dialled Numbers
+    4. Erase recent call list
+    5. Show call duration
+    6. Show call costs
+    7. Call cost settings
+    8. prepaid credit
+    select an option
+    """
+
+#SHOW_CALL_DURATION_MENU
+show_call_duration_menu = """
+CALL_DURATIONS
+    1. Last call duration
+    2. All calls Duration
+    3. Received call duration
+    4. Dialled calls duration
+    5. Clear timers
+    select an option
+    """
+
+#SHOW_CALL_COSTS_MENU
+show_call_costs_menu = """
+CALL_COSTS
+    1. Last call cost
+    2. All calls cost
+    5. Clear counters
+    select an option
+    """
+
+#CALL_COST_SETTINGS_MENU
+call_cost_settings_menu = """
+CALL_COST_SETTINGS
+    1. Call cost limit
+    2. Show cost in
+    select an option
+    """
+
+#TONES_MENU
+tones_menu = """
+TONES_MENU
+    1. Ringing tone
+    2. Ringing Volume
+    3. Incoming call alert
+    4. Composer
+    5. Message alert tone
+    6. Keypad tones
+    7. Warning and game tones
+    8. Vibrating alert
+    9. Screen saver
+    select an option
+    """
+
+#SETTINGS_MENU
+settings_menu = """
+SETTING_OPTIONS
+    1. Call Settings
+    2. Phone Settings
+    3. Security Settings
+    4. Restore Factory Settings
+    select an option
+    """
+
+#CALL_SETTINGS_MENU
+call_settings_menu = """
+CALL_SETTINGS
+    1. Automatic redial
+    2. Speed dialling
+    3. Call waiting option
+    4. Own number sending
+    5. Phone line in use
+    6. Automatic answer
+    select an option
+    """
+
+#PHONE_SETTINGS_MENU
+phone_settings_menu = """
+PHONE_SETTINGS
+    1. Language
+    2. Cell info display
+    3. Welcome note
+    4. Network Selection
+    5. Light
+    6. Confirm sim service actions
+    select an option
+    """
+
+#SECURITY_SETTINGS_MENU
+security_settings_menu = """
+SECURITY_SETTINGS
+    1. PIN Code request
+    2. Call barring service
+    3. Fixed dialing
+    4. Closed user group
+    5. Phone Security
+    6. Change access codes
+    select an option
+    """
+
+#RESTORE_FACTORY_SETTINGS_MENU
+restore_factory_settings_menu = """
+FACTORY_SETTINGS
+    1. Restore factory settings
+    """
+
+#CALL_DIVERT_MENU
+call_divert_menu = """
+CALL_DIVERT_SETTINGS
+    1. Call divert
+    """
+
+#GAMES_MENU
+games_menu = """
+GAMES
+    1. Snake xenza
+    """
+
+#CALCULATOR_MENU
+calculator_menu = """
+CALCULATOR_MENU
+    1. Calculator
+    """
+
+#REMINDER_MENU
+reminder_menu = """
+REMINDER_MENU
+    1. Reminder
+    """
+
+#CLOCK_MENU
+clock_menu = """
+CLOCK_SETTINGS
+    1. Alarm clock
+    2. Clock Settings
+    3. Date setting
+    4. Stopwatch
+    5. Countdown timer
+    6. Auto update of date and time
+    """
+
+#PROFILES_MENU
+profiles_menu = """
+PROFILE_SETTINGS
+    1. Profiles
+    """
+
+#SIM SERVICES_MENU
+sim_services_menu = """
+SIM_SERVICE_OPTION
+    1. SIM services
+    """
+print(sim_services_menu)
+
+
+
+
+
+
+
+
+
+
+
+
+
